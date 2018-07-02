d30cccdc58018644ad292df0c9a212200dc26940
Astik228 <serikovastik@mail.com>
2018-07-02 20:26:41 +0300

    First Commit

---
+++ IntroWinForms/NumbeOfArgsException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace IntroWinForms
+{
+    [Serializable]
+    internal class NumbeOfArgsException : Exception
+    {
+        public NumbeOfArgsException()
+        {
+        }
+
+        public NumbeOfArgsException(string message) : base(message)
+        {
+        }
+
+        public NumbeOfArgsException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NumbeOfArgsException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
